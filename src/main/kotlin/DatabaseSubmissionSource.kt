132fd82c91d869e686cbc3a9093026dc371012b9
Maplewing <sinmaplewing@gmail.com>
2020-09-14 21:44:31 +0800

    fix: DatabaseSubmissionSource should be Singleton & main thread need to be sleep more time

--- src/main/kotlin/DatabaseSubmissionSource.kt
+++ src/main/kotlin/DatabaseSubmissionSource.kt
@@ -6,7 +6,7 @@ import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.jetbrains.exposed.sql.update
 
-class DatabaseSubmissionSource: ISubmissionSource {
+object DatabaseSubmissionSource: ISubmissionSource {
     init {
         val config = HikariConfig("/hikari.properties")
         config.schema = "public"
